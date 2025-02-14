551c39cea62f6c9dee814527d344185ac91e29a7
Thomass055 <thomasmarchionni09@gmail.com>
2025-02-14 17:54:41 +0100

    esercizi python

---
+++ Lezione0.py/esercizi1.py
@@ -0,0 +1,56 @@
+# esercizio 2.3
+
+# name_p : str = "Thomas"
+# print(f"hello {name_p} would you like to learn some python today?")
+
+#esercizio 2.4
+
+# name_p : str = "Thomas"
+
+# print(name_p.lower())
+# print(name_p.upper())
+# print(name_p.title())
+
+# esercizio 2.5
+
+# author_p : str= "benedetta"
+# quote_p : str= "non ho paura delle istituzioni"
+
+# print(f"{author_p}, una volta ha detto {quote_p} !")
+
+# esercizio 2.6
+
+# famous_person : str= "benedetta"
+# message : str= "non ho paura delle istituzioni"
+
+# print(f"{famous_person}, una volta ha detto {message} !")
+
+#esercizio 3.1
+
+# list1: list = ["matteo","camilla","francesco"]
+# print (list1[0])
+# print (list1[1])
+# print (list1[2])
+
+#esercizio 3.2
+
+# list1: list = ["matteo","camilla","francesco"]
+
+# print(f"ciao {list1[0]}, come stai ?")
+# print(f"ciao {list1[1]}, come stai ?")
+# print(f"ciao {list1[2]}, come stai ?")
+
+#esercizio 3.3
+
+# cars_list : list = ["audi","ferrari","lamborghini"]
+
+# print(f"{cars_list[0]}, è il miglior marchio tedesco")
+# print(f"{cars_list[1]}, è il miglior marchio italiano")
+# print(f"{cars_list[2]}, è secondo solo a ferrari")
+
+#esercizio 3.4
+
+
+
+
+
