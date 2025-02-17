4c444dad940eef73d785a1b4d3fbd7ff2750773f
Thomass055 <thomasmarchionni09@gmail.com>
2025-02-17 12:12:56 +0100

    save

--- Lezione_02/esercizi.py
+++ Lezione_02/esercizi.py
@@ -31,3 +31,66 @@
 
 # ESERCIZIO 3.7
 
+# famous_person_list : list = ["Totti","Baggio","Del Piero"]
+# print(f"ciao, {famous_person_list[1]}, mi dispiace ma non posso invitarti alla cena")
+# famous_person_list.pop(1)
+
+# print(f"ciao {famous_person_list[0]}, la cena è confermata")
+# print(f"ciao {famous_person_list[1]}, la cena è confermata")
+
+# del famous_person_list [0]
+
+# #esercizio 3.3
+
+# cars_list : list = ["audi","ferrari","lamborghini"]
+
+# print(f"{cars_list[0]}, è il miglior marchio tedesco")
+# print(f"{cars_list[1]}, è il miglior marchio italiano")
+# print(f"{cars_list[2]}, è secondo solo a ferrari")
+
+# del famous_person_list [0]
+# print(f"famous_person_list: {famous_person_list}")
+
+# ESERCIZIO 3.8
+
+# geographic_list : list =["Rio De Janeiro" , "New York" , "Dubai" , "Los Angeles" , "Tokyo"]
+
+# print(f"Lista originale:\n{geographic_list}")
+# print(f"Lista ordinata:\n{sorted(geographic_list)}")
+
+# print(f"Lista originale:\n{geographic_list}")
+
+# print(f"Lista invertita: \n {sorted(geographic_list, reverse=True)}")
+
+# print(f"Lista originale:\n{geographic_list}")
+
+# geographic_list.reverse()
+
+# print(f"reverse:\n{geographic_list}")
+
+# geographic_list.reverse()
+
+# print(f"reverse:\n{geographic_list}")
+
+# geographic_list.sort()
+# print(f"Lista in ordine:\n {geographic_list}")
+
+# geographic_list.sort(reverse=True)
+# print(f"Lista invertita: \n{geographic_list}")
+
+#ESERCIZIO 3.9
+
+# famous_person_list : list = ["Totti","Baggio","Del Piero"]
+# print(f"A questa cena saranno prensenti {len(famous_person_list)} persone")
+
+#ESERCIZIO 3.10
+
+# list_language: list = ["Italiano" , "Francese" , "Spagnolo" , "Portoghese"]
+
+# list_language.insert(1,"Tedesco")
+# list_language.append("Polacco")
+# list_language.pop(3)
+# del list_language[2]
+# list_language.sort()
+# list_language.reverse()
+# print(sorted(list_language))
